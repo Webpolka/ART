0d212fdb5d7982f1809820ec36dadb21f0e6c877
Webpolka <webpolka@gmail.com>
2025-05-23 09:45:30 +0300

    First

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
-import { BaseHelpers } from './helpers/base-helpers';
+/* --------------------------------------------------------------------------------------------------------------------------
+BASE HELPERS
+-----------------------------------------------------------------------------------------------------------------------------*/
+import { BaseHelpers } from "./helpers/base-helpers";
 BaseHelpers.addLoadedClass();
 BaseHelpers.calcScrollbarWidth();
 BaseHelpers.addTouchClass();
+
+/* --------------------------------------------------------------------------------------------------------------------------
+BURGER CODE
+-----------------------------------------------------------------------------------------------------------------------------*/
+const burgerBtn = document.querySelector("#mobile-burger");
+const burgerClose = document.querySelector("#mobile-close");
+const mobileNav = document.querySelector("#mobile-nav");
+const body = document.querySelector("body");
+
+if (burgerBtn && burgerClose && mobileNav) {
+	burgerBtn.addEventListener("click", () => {
+		mobileNav.classList.toggle("show");
+		body.classList.toggle("no-scroll");
+	});
+
+	burgerClose.addEventListener("click", () => {
+		mobileNav.classList.remove("show");
+		body.classList.remove("no-scroll");
+	});
+
+	mobileNav.addEventListener("dblclick", () => {
+		mobileNav.classList.remove("show");
+		body.classList.remove("no-scroll");
+	});
+} else {
+	console.log("Mobile navigation HTML in DOM is broken !");
+}
+
+/* --------------------------------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------------------------------*/
